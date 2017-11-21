99c39f19b17bc9b5fabd54170139f60649652571
kullayaC <clear1_1@hotmail.com>
2017-11-21 15:19:51 +0700

    Testing Add Data

--- src/ooptest/HelloWorld.java
+++ src/ooptest/HelloWorld.java
@@ -4,6 +4,13 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world, I'm Bow");
 
+//        Add Data
+        System.out.println("Kullaya Chandet");
+        System.out.println("Information System");
+        System.out.println("Faculty of Management Technology");
+        System.out.println("RMUTSV");
+
+
 
     }//main
 
