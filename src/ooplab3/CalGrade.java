c129ef0a24ec528ef287c72f4b37f053a5b3113a
kullayaC <clear1_1@hotmail.com>
2017-11-29 11:41:28 +0700

    Testing Loopfor

--- src/ooplab3/CalGrade.java
+++ src/ooplab3/CalGrade.java
@@ -14,14 +14,22 @@ public class CalGrade {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int score;
-        System.out.print("Enter your score: ");
+        int count;
+        System.out.print("Enter your score 0-100: ");
         score = scanner.nextInt();
 //        test condition score
+        for (int i=1;i<=100)
         if (score<49) System.out.println("Your grade is F");
         else if (score<59) System.out.println("Your grade is D");
         else if (score<69) System.out.println("Your grade is C");
         else if (score<79) System.out.println("Your grade is B");
         else System.out.println("Your grade is A");
+        for (int score=0;score<=100;score++){
+            if (score<=3)
+                System.out.print("Your grade is ");
+
+
+        }//for
 
 
 
