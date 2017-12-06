d0697582c8d15875904b4b7ab3fad66bc2f96d31
kullayC <clear1_1@hotmail.com>
2017-12-06 09:48:48 +0700

    TestingWhileLoop

--- src/ooplab3/TestDoWhileLoop.java
+++ src/ooplab3/TestDoWhileLoop.java
@@ -1,4 +1,14 @@
 package ooplab3;
 
 public class TestDoWhileLoop {
-}
+    public static void main(String[] args) {
+//        do-while
+        int n = 1;
+        int count = 0;
+        do {
+            //statement
+            System.out.print(n + " ");
+        } while (++n <= 10);
+
+    }//main
+}//class
