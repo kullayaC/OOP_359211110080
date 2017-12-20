911412677300e929964b4a16532f9fe76dcb38a1
kullayaC <clear1_1@hotmail.com>
2017-12-20 08:56:54 +0700

    sortingArray

--- src/ooplab5/sortingArray.java
+++ src/ooplab5/sortingArray.java
@@ -1,78 +1,91 @@
 package ooplab5;
 
-import java.sql.Array;
+import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Scanner;
 
 public class sortingArray {
-    private static  final int MAX = 10;
-    private static int number []= new  int[MAX];
+    private static final int MAX = 10;
+    private static int number[] = new int[MAX];
     public static void main(String[] args) {
-        inputData (number,number.length);
+        inputData(number,number.length);
 
-        showData (number,number.length);
-        findMaxData (number);
-        findMinData (number);
-        System.out.println("Before sortiong: ");
-        sortingData (number);
-
+        showData(number, number.length);
+        findMaxData(number);
+        findMinData(number);
+        System.out.println("Before sorting: ");
         //ascending order
         sortingDataAscending(number);
+
         //descending order
-        sortingDataDescending(number);
+        sortingDataDesending(number);
 
     }//main
 
-    private static void sortingDataDescending(int[] number) {
-        System.out.println("Descending order: ");
-       // Arrays.sort(number, Collections.reverseOrder());
-        showData(number,number.length);
-    }//sortingDataDescending
+    private static void sortingDataDesending(int[] number) {
+        System.out.println("Desending Order: ");
+        Integer[] num = new Integer[number.length];
+        int i = 0;
+        for(int val:number){
+            num[i++] = val;
+        }
+
+        Arrays.sort(num, Collections.reverseOrder());
+        showData(num,num.length);
+    }//sortingDataDesending
 
     private static void sortingDataAscending(int[] number) {
-        System.out.println("Ascending order: ");
+        System.out.println("Ascending Order: ");
         Arrays.sort(number);
         showData(number,number.length);
-    }//sortingDataAscending
-
-
-    private static void sortingData(int[] number) {
     }//sortingData
 
     private static void findMinData(int[] number) {
         int min = number[0];
-        for (int i=0;i<number.length;i++) {
-            if (min >= number[i])
+        for (int i=0;i<number.length;i++){
+            if (min>=number[i])
                 min = number[i];
         }//for
-        System.out.println("The maximun data: "+min);
-    }//findMindata
+        System.out.println("The minimum data: " + min);
+    }//findMinData
 
     private static void findMaxData(int[] number) {
         int max = number[0];
-        for (int i=0;i<number.length;i++) {
-            if (max <= number[i])
+        for (int i=0;i<number.length;i++){
+            if (max<=number[i])
                 max = number[i];
         }//for
-        System.out.println("The maximun data: "+max);
-    }//findmax
+        System.out.println("The maximum data: " + max);
 
+    }//findMaxData
 
     private static void showData(int[] number, int length) {
         System.out.print("Data in array: ");
-        for (int i=0;i<length;i++){
+        for(int i=0;i<length;i++){
             System.out.print(number[i]+" ");
         }
         System.out.println();
-    }//showdata
+
+    }//showData
+    private static void showData(Integer[] number, int length) {
+        System.out.print("Data in array: ");
+        for(int i=0;i<length;i++){
+            System.out.print(number[i]+" ");
+        }
+        System.out.println();
+
+    }//showData
 
     private static void inputData(int[] number, int length) {
+        //user input data
         Scanner scanner = new Scanner(System.in);
         for (int i=0;i<length;i++)
         {
-            System.out.println("Enter an integer["+i+"]: ");
-            number[i]=scanner.nextInt();
-    }
-}//inputdata
+            System.out.print("Enter an integer["+i+"]: ");
+            number[i] = scanner.nextInt();
+        }
+
+
+    }//inputData
 }//class
