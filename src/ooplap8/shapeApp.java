a2bc69a51759faaf0bbf57ba2701017265ac4a87
kullayaC <clear1_1@hotmail.com>
2018-02-01 18:20:56 +0700

    interface : triangle

--- src/ooplap8/shapeApp.java
+++ src/ooplap8/shapeApp.java
@@ -5,5 +5,10 @@ public class shapeApp {
         Rectangle rectangle= new Rectangle(20,30);
         System.out.println(rectangle.toString());
         System.out.println("Object area :"+rectangle.getArea());
+
+        Triangle triangle=new Triangle(15,30);
+        System.out.println(triangle.toString());
+        System.out.println("Object area::"+triangle.getArea());
+
     }
 }
